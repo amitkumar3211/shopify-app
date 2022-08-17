7f81c2e983b1bde40a8e8377c7af9efdcbc9ceba
amitkumar3211 <amit.ewb@gmail.com>
2022-08-17 16:25:17 +0530

    outh completed

---
+++ web/frontend/pages/index.jsx
@@ -0,0 +1,45 @@
+import {
+  Card,
+  Page,
+  Layout,
+  TextContainer,
+  Image,
+  Stack,
+  Link,
+  Heading,
+} from "@shopify/polaris";
+import { TitleBar } from "@shopify/app-bridge-react";
+
+import { trophyImage } from "../assets";
+
+import { ProductsCard } from "../components";
+
+export default function HomePage() {
+  return (
+    <Page narrowWidth>
+      <TitleBar title="Shopify App" primaryAction={null} />
+      <Layout>
+        <Layout.Section>
+          <Card sectioned>
+            <Stack
+              wrap={false}
+              spacing="extraTight"
+              distribution="trailing"
+              alignment="center"
+            >
+              <Stack.Item fill>
+                <TextContainer spacing="loose">
+                  <Heading>Nice work on building a Shopify app 🎉</Heading>
+                  
+                </TextContainer>
+              </Stack.Item>
+            </Stack>
+          </Card>
+        </Layout.Section>
+        <Layout.Section>
+          <ProductsCard />
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
+}
